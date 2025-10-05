6e6c407a576a8ed712088f7f50770b4c3c8dacbb
Lalman <lalmanchaudhary2@gmail.com>
2025-10-05 11:32:37 +0530

    integrated API to fetch weather data from openweather.org

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const api = "2086b6e0fd622d44435eb1907e78d46c95";
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=germany&appid=86b6e0fd622d44435eb1907e78d46c95";
+const apikey = "86b6e0fd622d44435eb1907e78d46c95";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+  var data = await response.json();
+
+  console.log(data);
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
